55f47ac4c29fbf6e7ed6006c9673ce1ca1772bf4
Rezlang <96161550+Rezlang@users.noreply.github.com>
2023-08-09 16:41:04 -0400

    Mostly functional FCFS

--- project.py
+++ project.py
@@ -29,21 +29,29 @@ class Status(Enum):
     RUNNING = 2
     WAITING = 3
     SWITCHING_OUT = 4
+    TERMINATED = 5
 
 class Process:
     def __init__(self):
         self.id = ''
         self.arrivalTime = 0
         self.burstTimes: List[List[int]] = []
+        self.waitTime = 0
         self.isCPUBound = False
         self.status = Status.UNARRIVED
         self.switchingTimer = 0
+        self.numContextSwitches = 0
+        self.numBursts = 0
 
 class manager:
     def __init__(self, processes):
         self.generatedProcesses: List[Process] = processes
         self.readyQueue: List[Process] = []
         self.activeProcess: Process = self.generatedProcesses[0]
+        self.busyTime = 0
+        self.avgCPUBurst = sum(sum(q[0] for q in p.burstTimes) for p in self.generatedProcesses) / sum(len(p.burstTimes) for p in self.generatedProcesses)
+        self.maxCPUBurst = max(sum(q[0] for q in p.burstTimes) / len(p.burstTimes) for p in self.generatedProcesses)
+        self.minCPUBurst = min(sum(q[0] for q in p.burstTimes) / len(p.burstTimes) for p in self.generatedProcesses)
 
 def FirstComeFirstServe(processes, contextSwitchTime):
     print("time 0ms: Simulator started for FCFS [Q <empty>]")
@@ -61,44 +69,65 @@ def FirstComeFirstServe(processes, contextSwitchTime):
             if PM.activeProcess.switchingTimer == 0:
                 PM.activeProcess.status = Status.RUNNING
                 # time 28ms: Process C started using the CPU for 2920ms burst [Q <empty>]
-                print("time ", time, "ms: Process "+PM.activeProcess.id + " started using the CPU for ",
-                      PM.activeProcess.burstTimes[0][0], "ms burst ", end="", sep='')
+                if time < 10000:
+                    print("time ", time, "ms: Process "+PM.activeProcess.id + " started using the CPU for ",
+                        PM.activeProcess.burstTimes[0][0], "ms burst ", end="", sep='')
 
-                if len(PM.readyQueue) != 0:
-                    print("[Q ", " ".join(x.id for x in PM.readyQueue), "]", sep='')
-                else:
-                    print("[Q <empty>]")
+                    if len(PM.readyQueue) != 0:
+                        print("[Q ", " ".join(x.id for x in PM.readyQueue), "]", sep='')
+                    else:
+                        print("[Q <empty>]")
             else:
                 PM.activeProcess.switchingTimer -= 1
 
         elif PM.activeProcess.status == Status.SWITCHING_OUT:
             if PM.activeProcess.switchingTimer == 0:
-                PM.activeProcess.status = Status.WAITING
-                PM.activeProcess = PM.readyQueue[0]
-                PM.readyQueue.pop(0)
-                PM.activeProcess.status = Status.SWITCHING_IN
-                PM.activeProcess.switchingTimer = contextSwitchTime / 2 - 1
+                if (PM.activeProcess.burstTimes[0][1] == 0):
+                    PM.activeProcess.status = Status.TERMINATED
+                else:
+                    PM.activeProcess.status = Status.WAITING
+                    if len(PM.readyQueue) != 0:
+                        PM.activeProcess = PM.readyQueue[0]
+                        PM.readyQueue.pop(0)
+                        PM.activeProcess.numContextSwitches += 1
+                        PM.activeProcess.status = Status.SWITCHING_IN
+                        PM.activeProcess.switchingTimer = contextSwitchTime / 2 - 1
             else:
                 PM.activeProcess.switchingTimer -= 1
 
+        if (PM.activeProcess.status == Status.WAITING or PM.activeProcess.status == Status.READY or PM.activeProcess.status == Status.TERMINATED) and len(PM.readyQueue) != 0:
+            PM.activeProcess = PM.readyQueue[0]
+            PM.readyQueue.pop(0)
+            PM.activeProcess.numContextSwitches += 1
+            PM.activeProcess.status = Status.SWITCHING_IN
+            PM.activeProcess.switchingTimer = contextSwitchTime / 2 - 1
+
+
         if PM.activeProcess.status == Status.RUNNING:
             if PM.activeProcess.burstTimes[0][0] == 0:
-                print("time ", time, "ms: Process "+PM.activeProcess.id + " completed a CPU burst; ",
-                      len(PM.activeProcess.burstTimes)-1, " bursts to go ", end="", sep='')
+                if len(PM.activeProcess.burstTimes) == 1:
+                    print("time ", time, "ms: Process ", PM.activeProcess.id, " terminated ", sep="", end="")
+                    if len(PM.readyQueue) != 0:
+                        print("[Q " + " ".join(x.id for x in PM.readyQueue) + "]", sep='')
+                    else:
+                        print("[Q <empty>]")
+                elif time < 10000:
+                    print("time ", time, "ms: Process "+PM.activeProcess.id + " completed a CPU burst; ",
+                        len(PM.activeProcess.burstTimes)-1, " bursts to go ", end="", sep='')
 
-                if len(PM.readyQueue) != 0:
-                    print(
-                        "[Q " + " ".join(x.id for x in PM.readyQueue) + "]", sep='')
-                else:
-                    print("[Q <empty>]")
+                    if len(PM.readyQueue) != 0:
+                        print(
+                            "[Q " + " ".join(x.id for x in PM.readyQueue) + "]", sep='')
+                    else:
+                        print("[Q <empty>]")
 
-                print("time ", time, "ms: Process " + PM.activeProcess.id + " switching out of CPU; blocking on I/O until time ",
-                      (time + PM.activeProcess.burstTimes[0][1]), "ms ", end="", sep='')
-                if len(PM.readyQueue) != 0:
-                    print(
-                        "[Q " + " ".join(x.id for x in PM.readyQueue) + "]", sep='')
-                else:
-                    print("[Q <empty>]")
+                    print("time ", time, "ms: Process " + PM.activeProcess.id + " switching out of CPU; blocking on I/O until time ",
+                        (time + PM.activeProcess.burstTimes[0][1] + contextSwitchTime // 2), "ms ", end="", sep='')
+                    if len(PM.readyQueue) != 0:
+                        print(
+                            "[Q " + " ".join(x.id for x in PM.readyQueue) + "]", sep='')
+                    else:
+                        print("[Q <empty>]")
 
                 PM.activeProcess.switchingTimer = contextSwitchTime / 2 - 1
                 PM.activeProcess.status = Status.SWITCHING_OUT
@@ -109,33 +138,68 @@ def FirstComeFirstServe(processes, contextSwitchTime):
         for p in PM.generatedProcesses:
             if p.status == Status.WAITING:
                 if p.burstTimes[0][1] == 0:
-                    p.status = Status.READY
                     p.burstTimes.pop(0)
-                    PM.readyQueue.append(p)
-                    # time 3318ms: Process C completed I/O; added to ready queue [Q B C]
-                    print("time ", time, "ms: Process " + p.id +
-                          " completed I/O; added to ready queue ", end="", sep='')
-                    if len(PM.readyQueue) != 0:
-                        print(
-                            "[Q ", " ".join(x.id for x in PM.readyQueue), "]", sep='')
+                    if len(PM.readyQueue) == 0 and PM.activeProcess.status != Status.RUNNING:
+                        PM.activeProcess = p
+                        PM.activeProcess.numContextSwitches += 1
+                        p.status = Status.SWITCHING_IN
+                        p.switchingTimer = contextSwitchTime / 2 - 1
                     else:
-                        print("[Q <empty>]")
+                        p.status = Status.READY
+                        PM.readyQueue.append(p)
+                    # time 3318ms: Process C completed I/O; added to ready queue [Q B C]
+                    if time < 10000:
+                        print("time ", time, "ms: Process " + p.id +
+                            " completed I/O; added to ready queue ", end="", sep='')
+                        if len(PM.readyQueue) != 0:
+                            print(
+                                "[Q ", " ".join(x.id for x in PM.readyQueue), "]", sep='')
+                        else:
+                            print("[Q ",p.id if p.status == Status.SWITCHING_IN else "<empty>" , "]")
                 else:
                     p.burstTimes[0][1] -= 1
 
         # Checks if new processes have arrived
         for p in PM.generatedProcesses:
+            if p.status == Status.RUNNING:
+                PM.busyTime += 1
+            if p.status == Status.WAITING:
+                p.waitTime += 1
+
             if p.arrivalTime <= time and p.status == Status.UNARRIVED:
                 PM.readyQueue.append(p)
-                print("time ", time, "ms: Process " + p.id +
-                      " arrived; added to ready queue [Q ", " ".join(x.id for x in PM.readyQueue), "]", sep='')
+                if time < 10000:
+                    print("time ", time, "ms: Process " + p.id +
+                        " arrived; added to ready queue [Q ", " ".join(x.id for x in PM.readyQueue), "]", sep='')
                 p.status = Status.READY
         done = True
         for p in PM.generatedProcesses:
-            if len(p.burstTimes) != 0:
+            if p.status != Status.TERMINATED:
                 done = False
 
-        time += 1
+        time += 1 if not done else 0
+    #time 324063ms: Simulator ended for FCFS [Q <empty>]
+    print("time ",time,"ms: Simulator ended for FCFS [Q <empty>]", sep="")
+    fn = "simout.txt"      
+    fp = open(fn, "a")
+#     Algorithm FCFS
+# -- CPU utilization: 84.253%
+# -- average CPU burst time: 3067.776 ms (4071.000 ms/992.138 ms)
+# -- average wait time: 779.663 ms (217.284 ms/1943.207 ms)
+# -- average turnaround time: 3851.439 ms (4292.284 ms/2939.345 ms)
+# -- number of context switches: 89 (60/29)
+# -- number of preemptions: 0 (0/0)
+# TODO: First number in parenthesis is for CPU-bound processes, the second is for IO-bound processes
+
+    print("Algorithm FCFS", file=fp)
+    print("-- CPU utililization: {:.3f}%".format(PM.busyTime / time * 100), file=fp)
+    print("-- average CPU burst time: {:.3f} ms ({:.3f} ms/{:.3f} ms)".format(PM.avgCPUBurst, PM.maxCPUBurst, PM.minCPUBurst), file=fp)
+    # avgWaits = [p.waitTime / p.numBursts for p in PM.generatedProcesses]
+    # tmp = sum(p.waitTime for p in PM.generatedProcesses) / sum(p.numBursts for p in PM.generatedProcesses)
+    # print("-- average wait time: {:.3f} ms ({:.3f} ms/{:.3f} ms)".format( tmp / len(PM.generatedProcesses), max(avgWaits), min(avgWaits)), file = fp)
+    print("-- number of context switches: {:d} ({:d}/{:d})".format())
+    fp.close()
+
 
 def next_exp(lambda_: float, upperBound: int) -> int:
     num = upperBound
@@ -148,15 +212,9 @@ def partOneOutput(processes: List[Process], numCPUProc: int) -> int:
           str(numCPUProc) + ' CPU-bound ' + ("process >>>" if numCPUProc == 1 else "processes >>>"))
     for i in range(len(processes)):
         print(('CPU-bound process ' if processes[i].isCPUBound else 'I/O-bound process ') + str(processes[i].id) + ': arrival time ' + str(
-            processes[i].arrivalTime) + 'ms; ' + str(len(processes[i].burstTimes)) + (' CPU burst:' if len(processes[i].burstTimes) == 1 else ' CPU bursts:'))
-    #     the below output is only for part I
-    #     for j in range(len(processes[i].burstTimes)):
-    #         if j != len(processes[i].burstTimes) - 1:
-    #             print('--> CPU burst ' + str(processes[i].burstTimes[j][0]) +
-    #                   'ms --> I/O burst ' + str(processes[i].burstTimes[j][1]) + 'ms')
-    #         else:
-    #             print('--> CPU burst ' +
-    #                   str(processes[i].burstTimes[j][0]) + 'ms')
+            processes[i].arrivalTime) + 'ms; ' + str(len(processes[i].burstTimes)) + (' CPU burst' if len(processes[i].burstTimes) == 1 else ' CPU bursts'))
+    
+    print()
     return 0
 
 def main(argv):
@@ -188,6 +246,7 @@ def main(argv):
         p.id = chr(65 + i)
         p.arrivalTime = math.floor(next_exp(lambda_, upperBound)) - 1
         numCPUBursts = math.ceil(randomizer.drand() * 64)
+        p.numBursts = (numCPUBursts * 2) - 1
         if p.isCPUBound:
             for j in range(numCPUBursts - 1):
                 p.burstTimes.append([math.ceil(next_exp(lambda_, upperBound)) * 4,
